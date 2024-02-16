737ab7820c2690fc87e26ead1951f9962c59bd72
Matheus Vitor <vitor.lp45@gmail.com>
2024-02-16 19:33:33 -0300

    feat: request logger

--- src/infra/middlewares/index.ts
+++ src/infra/middlewares/index.ts
@@ -2,11 +2,13 @@ import cors from "cors";
 import { json, urlencoded } from "express";
 import { authorization } from "@/infra/middlewares/authorization";
 import { security } from "@/infra/middlewares/security";
+import { requestLogger } from "@/infra/middlewares/requests-logger";
 
 export const middlewares = [
 	json(),
 	urlencoded({extended: false}),
 	cors(),
 	security,
-	authorization
+	authorization,
+	requestLogger
 ]
---
+++ src/infra/middlewares/requests-logger.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express"
+
+export const requestLogger = async (request: Request, response: Response, next: NextFunction) => {
+
+	const logText = `[${request.method}] - ${request.url} - ${request.ip} - ${new Date().toLocaleDateString('pt-BR', {hour: "2-digit", minute: '2-digit'})} - ${request.body && '[BODY]' + JSON.stringify(request.body)}}`
+
+
+	console.log(logText);
+	next();
+}
