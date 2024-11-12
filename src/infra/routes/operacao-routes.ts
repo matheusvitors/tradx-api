79dee13bf12aee145642a52ba2fd6b91ac2713d2
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2024-11-12 16:54:19 -0300

    logica de upload de arquivo

--- src/infra/routes/operacao-routes.ts
+++ src/infra/routes/operacao-routes.ts
@@ -1,13 +1,26 @@
 import { Router, Request, Response} from 'express'
+import multer from 'multer';
 import { ativosPrismaRepository, contaPrismaRepository, operacaoPrismaRepository } from '@/infra/database/prisma';
 import { route } from '@/infra/adapters/route';
 import { createOperacaoController, editOperacaoController, getOperacaoController, importOperacoesByCsvController, listOperacaoByContaController, listOperacaoController, removeOperacaoController } from '@/application/controllers/operacao';
+import { notFound } from '@/infra/adapters/response-wrapper';
 
 const router = Router();
 const repository = operacaoPrismaRepository;
 const contaRepository = contaPrismaRepository;
 const ativoRepository = ativosPrismaRepository;
 const path = '/operacoes'
+
+const storage = multer.diskStorage({
+	destination: function (request, file, callback) {
+		callback(null, 'temp/');
+	},
+	filename: function (request, file, callback) {
+		const extension = file.originalname.split('.');
+		callback(null, `operacoes.${extension[1]}`);
+	},
+})
+const upload = multer({storage});
 
 router.get(`${path}`, async (request: Request, response: Response) => {
 	const responseData = await listOperacaoController(repository);
@@ -46,13 +59,18 @@ router.post(`${path}`, async (request: Request, response: Response) => {
 	return route({ response, responseData });
 })
 
-router.post(`${path}/import`, async (request: Request, response: Response) => {
+router.post(`${path}/import`, upload.single('csvFile'), async (request: Request, response: Response) => {
+	if(!request.file) {
+		return route({ response, responseData: notFound('Arquivo não encontrado') });
+	}
+
 	const responseData = await importOperacoesByCsvController({
 		operacaoRepository: repository,
 		ativoRepository,
 		contaRepository,
-		csvFile: ''
+		csvFile: request.file.filename
 	})
+
 	return route({ response, responseData });
 })
 
