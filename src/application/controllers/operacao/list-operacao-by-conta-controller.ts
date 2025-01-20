9b612b331d481e4a3ea2a1a35f12e3d5d2468b5d
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2025-01-20 14:16:13 -0300

    fix: range de data pegar até as 23:59

--- src/application/controllers/operacao/list-operacao-by-conta-controller.ts
+++ src/application/controllers/operacao/list-operacao-by-conta-controller.ts
@@ -15,10 +15,12 @@ interface ListOperacaoByContaControllerParams {
 
 export const listOperacaoByContaController = async (params: ListOperacaoByContaControllerParams): Promise<ResponseData> => {
 	try {
+
 		const { repository, contaId, range } = params;
+		console.log({ gte: new Date(`${range.init} 00:00`), lte: new Date(`${range.end} 23:59`)});
 		const operacoes = await repository.filter!([
 			{ field: 'contaId', value: contaId },
-			{ field: 'dataEntrada', value: { gte: new Date(range.init), lte: new Date(range.end)}}
+			{ field: 'dataEntrada', value: { gte: new Date(`${range.init} 00:00`), lte: new Date(`${range.end} 23:59`)}}
 		]);
 
 		if(!operacoes) {
